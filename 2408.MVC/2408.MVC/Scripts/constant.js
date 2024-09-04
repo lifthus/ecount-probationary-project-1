80421565733d1fcb205f28ba2465968832ccc9aa
lifthus <lifthus531@gmail.com>
2024-09-04 10:38:47 +0900

    feat: 품목 생성 완료

--- 2408.MVC/2408.MVC/Scripts/constant.js
+++ 2408.MVC/2408.MVC/Scripts/constant.js
@@ -1,8 +1,10 @@
 export const MAIN_PATH = "/";
 export const ITEM_PATH = "/product";
-export const ITEM_CREATE_POPUP_PATH = "popup/product/create";
-export const ITEM_UPDATE_POPUP_PATH = "popup/product/update";
-export const ITEM_SELECT_POPUP_PATH = "popup/product/select";
+export const ITEM_CREATE_POPUP_PATH = "/popup/product/create";
+export const ITEM_UPDATE_POPUP_PATH = "/popup/product/update";
+export const ITEM_SELECT_POPUP_PATH = "/popup/product/select";
 export const SALE_PATH = "/sale";
 
-export const API_PRODUCT_SELECT_PATH = "/api/product";
+export const API_PRODUCT_BASE_PATH = "/api/product";
+export const API_PRODUCT_CREATE_PATH = API_PRODUCT_BASE_PATH;
+export const API_PRODUCT_SELECT_PATH = API_PRODUCT_BASE_PATH;
